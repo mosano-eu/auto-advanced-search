29cd5686f6ab08eb94f0f2f2a3007f8208fcf047
brunocasanova <deftonicks_me@hotmail.com>
2015-02-09 17:30:28 +0000

    Add some testing cases

--- test/tests.js
+++ test/tests.js
@@ -5,12 +5,56 @@ var query = require( '../' ),
 
 describe( "auto-advanced-search", function() {
 
-	describe( "query", function() {
+	function regExpTest( string ){
 
-		it( 'should return string' );
+		if( typeof string != 'string' ){
+			return false;
+		}
+
+		if( ! /([\+\-\<\>])?(^[a-z]{1,25}|[a-z]{4,25}|\"[a-z]{4,25}(?:\ [a-z]{1,25})+")/.test( string ) ){
+			return false;
+		}
+
+		return string;
+	};
+
+	describe( "accepted string", function() {
+		var query = 'quinta de sao jose';
+
+		it( 'should be a string', function (){
+
+			expect( query ).to.be.a( 'string' );
+
+		});
+
+		it( 'should match the regexp', function (){
+
+			expect( query ).to.match( /([\+\-\<\>])?(^[a-z]{1,25}|[a-z]{4,25}|\"[a-z]{4,25}(?:\ [a-z]{1,25})+")/ );
+
+		});
+	
+	});
+
+	describe( "not a string", function() {
+		var query = 4;
+
+		var test = regExpTest( query );
+
+		it( 'should return false if it isnt a string', function (){
+
+			expect( test ).to.be.a( 'boolean' );
+			expect( test ).to.be.false;
+
+		});
+
+		it( 'should not match if it isnt an expected query', function (){
+
+			expect( query ).not.to.match( /([\+\-\<\>])?(^[a-z]{1,25}|[a-z]{4,25}|\"[a-z]{4,25}(?:\ [a-z]{1,25})+")/ );
+
+		});
 
 	});
-	
+
 });
 
 
