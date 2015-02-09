e9e1fe866f493ebea0811e345645804dac10e9a5
brunocasanova <deftonicks_me@hotmail.com>
2015-02-09 22:25:38 +0000

    Add main function and some test cases

--- test/tests.js
+++ test/tests.js
@@ -1,58 +1,104 @@
-var query = require( '../' ),
+var aas = require( '../' ),
 	chai = require( 'chai' ),
 	expect = chai.expect,
 	sinon = require( 'sinon' );
 
 describe( "auto-advanced-search", function() {
 
-	function regExpTest( string ){
+	describe( "first string can contain 1 caracter", function() {
+		var string = 'x',
+			fn = aas( string );
 
-		if( typeof string != 'string' ){
-			return false;
-		}
-
-		if( ! /([\+\-\<\>])?(^[a-z]{1,25}|[a-z]{4,25}|\"[a-z]{4,25}(?:\ [a-z]{1,25})+")/.test( string ) ){
-			return false;
-		}
-
-		return string;
-	};
-
-	describe( "accepted string", function() {
-		var query = 'quinta de sao jose';
-
-		it( 'should be a string', function (){
-
-			expect( query ).to.be.a( 'string' );
-
+		it( 'should exist and return a string', function (){
+			expect( fn ).to.be.ok;
+			expect( fn ).to.be.a( 'string' );
 		});
 
-		it( 'should match the regexp', function (){
+		it( 'should have a length of 2', function (){
+			// The length is 2 because the plus have been added here.
+			expect( fn ).to.have.length( 2 );
+		});
 
-			expect( query ).to.match( /([\+\-\<\>])?(^[a-z]{1,25}|[a-z]{4,25}|\"[a-z]{4,25}(?:\ [a-z]{1,25})+")/ );
-
+		it( 'should return one string with a plus in the begining', function (){
+			expect( fn ).to.equal( '+x' );
 		});
 	
 	});
 
-	describe( "not a string", function() {
-		var query = 4;
+	describe( "first string can contain 2 caracters", function() {
+		var string = 'xx',
+			fn = aas( string );
 
-		var test = regExpTest( query );
-
-		it( 'should return false if it isnt a string', function (){
-
-			expect( test ).to.be.a( 'boolean' );
-			expect( test ).to.be.false;
-
+		it( 'should exist and return a string', function (){
+			expect( fn ).to.be.ok;
+			expect( fn ).to.be.a( 'string' );
 		});
 
-		it( 'should not match if it isnt an expected query', function (){
-
-			expect( query ).not.to.match( /([\+\-\<\>])?(^[a-z]{1,25}|[a-z]{4,25}|\"[a-z]{4,25}(?:\ [a-z]{1,25})+")/ );
-
+		it( 'should have a length of 3', function (){
+			// The length is 3 because the plus have been added here.
+			expect( fn ).to.have.length( 3 );
 		});
 
+		it( 'should return one string with a plus in the begining', function (){
+			expect( fn ).to.equal( '+xx' );
+		});
+	
+	});
+
+	describe( "first string can contain 3 caracters", function() {
+		var string = 'xxx',
+			fn = aas( string );
+
+		it( 'should exist and return a string', function (){
+			expect( fn ).to.be.ok;
+			expect( fn ).to.be.a( 'string' );
+		});
+
+		it( 'should have a length of 4', function (){
+			// The length is 4 because the plus have been added here.
+			expect( fn ).to.have.length( 4 );
+		});
+
+		it( 'should return one string with a plus in the begining', function (){
+			expect( fn ).to.equal( '+xxx' );
+		});
+	
+	});
+
+	describe( "one string factor", function() {
+		var string = 'guarda sol';
+
+		it( 'should exist and return a string', function (){
+			var fn = aas( string );
+
+			expect( fn ).to.be.ok;
+			expect( fn ).to.be.a( 'string' );
+		});
+
+		it( 'should return one strings with plus in the begining of each', function (){
+			var fn = aas( string );
+
+			expect( fn ).to.equal( '+guarda' );
+		});
+	
+	});
+
+	describe( "two strings factor", function() {
+		var string = 'quinta de sao jose';
+
+		it( 'should exist and return a string', function (){
+			var fn = aas( string );
+
+			expect( fn ).to.be.ok;
+			expect( fn ).to.be.a( 'string' );
+		});
+
+		it( 'should return two strings with plus in the begining of each', function (){
+			var fn = aas( string );
+
+			expect( fn ).to.equal( '+quinta +jose' );
+		});
+	
 	});
 
 });
